31b40831cbf4d24c238ca385f11808d97820c032
AvdL <55466113+AdvdLaar@users.noreply.github.com>
2024-02-07 23:33:57 +0100

    Add files via upload
    First release

---
+++ VPN_Active/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace VPN_Active
+{
+    //Size form 589; 207
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoStartAtWindowsStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check again";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(422, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "operator";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "No VPN hosts :";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 119);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(567, 32);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.Black;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(162, 25);
+            this.toolStripStatusLabel1.Text = "External host name";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(567, 33);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.autoStartAtWindowsStartupToolStripMenuItem,
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(73, 29);
+            this.menuToolStripMenuItem.Text = "&Menu";
+            // 
+            // autoStartAtWindowsStartupToolStripMenuItem
+            // 
+            this.autoStartAtWindowsStartupToolStripMenuItem.CheckOnClick = true;
+            this.autoStartAtWindowsStartupToolStripMenuItem.Enabled = false;
+            this.autoStartAtWindowsStartupToolStripMenuItem.Name = "autoStartAtWindowsStartupToolStripMenuItem";
+            this.autoStartAtWindowsStartupToolStripMenuItem.Size = new System.Drawing.Size(438, 34);
+            this.autoStartAtWindowsStartupToolStripMenuItem.Text = "&Auto start at windows startup";
+            this.autoStartAtWindowsStartupToolStripMenuItem.Click += new System.EventHandler(this.autoStartAtWindowsStartupToolStripMenuItem_Click);
+            // 
+            // enableSoundsWhenConnectionChangesToolStripMenuItem
+            // 
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem.CheckOnClick = true;
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem.Enabled = false;
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem.Name = "enableSoundsWhenConnectionChangesToolStripMenuItem";
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem.Size = new System.Drawing.Size(438, 34);
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem.Text = "Enable &sounds when connection changes";
+            this.enableSoundsWhenConnectionChangesToolStripMenuItem.Click += new System.EventHandler(this.enableSoundsWhenConnectionChangesToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(438, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 10000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 151);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VPN Active";
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+#endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoStartAtWindowsStartupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer3;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem enableSoundsWhenConnectionChangesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ VPN_Active/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using Microsoft.Win32;
+
+namespace VPN_Active
+{
+    public partial class Main : Form
+    {
+        public static Main It;   // Singleton.
+        public String ExternalHostName = "";
+        string RegistryApplicationKey = "";
+        string IPNumber = "";
+        const double TIMEOUT = 500; // milliseconds
+
+        public Main()
+        {
+            InitializeComponent();
+            It = this;
+            RegistryApplicationKey = "SOFTWARE\\" + Application.ProductName;
+            NetworkChange.NetworkAddressChanged += new
+            NetworkAddressChangedEventHandler(AddressChangedCallback);
+            notifyIcon1.Icon = Properties.Resources.VPN_blue;
+            toolStripStatusLabel1.ForeColor = Color.Black;
+            toolStripStatusLabel1.Text = "";
+            autoStartAtWindowsStartupToolStripMenuItem.Checked = CheckStartup();
+            autoStartAtWindowsStartupToolStripMenuItem.Enabled = true;
+            enableSoundsWhenConnectionChangesToolStripMenuItem.Checked = GetBoolValue("PlaySound", true);
+            enableSoundsWhenConnectionChangesToolStripMenuItem.Enabled = true;
+            string[] args = Environment.GetCommandLineArgs();
+            if ((args.Length==2) && (args[1]=="/minimize"))
+            {
+                timer2.Enabled = true;
+            }
+            RestartTimer();
+            textBox1.Text = GetStringValue("NoVPN", "operator");
+            textBox1.TextChanged += textBox1_TextChanged;
+        }
+
+        private void RestartTimer()
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new Action(delegate
+                {
+                    RestartTimer();
+                }));
+                return;
+            }
+            timer1.Stop();
+            timer1.Start();
+        }
+
+        private bool StoreBoolValue(string ValueStr, Boolean Bool)
+        {
+            bool ret = false;
+            string DefaultStr = "False";
+            Boolean StoredValue;
+            RegistryKey rkey = Registry.CurrentUser.CreateSubKey(RegistryApplicationKey);
+            if (rkey != null)
+            {
+                if (Bool)
+                {
+                    DefaultStr = "False";
+                }
+                else
+                {
+                    DefaultStr = "True";
+                }
+                StoredValue = Convert.ToBoolean(rkey.GetValue(ValueStr, DefaultStr));
+                if (Bool != StoredValue) rkey.SetValue(ValueStr, Bool);
+                return (true);
+            }
+            return ret;
+        }
+
+        private Boolean GetBoolValue(string ValueStr, Boolean Default)
+        {
+            string DefaultStr = "False";
+            bool Result = Default;
+            RegistryKey rkey = Registry.CurrentUser.OpenSubKey(RegistryApplicationKey);
+            if (rkey != null)
+            {
+                if (Default) DefaultStr = "True";
+                Result = Convert.ToBoolean(rkey.GetValue(ValueStr, DefaultStr));
+            }
+            return (Result);
+        }
+
+        private void SetStartup(bool Enable)
+        {
+            string AppName = Application.ProductName;
+            string Args = "/minimize";
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (Enable)
+            {
+                if (Args != "")
+                {
+                    rk.SetValue(AppName, "\"" + Application.ExecutablePath + "\" /minimize");
+                }
+                else
+                {
+                    rk.SetValue(AppName, Application.ExecutablePath);
+                }
+            }
+            else
+                rk.DeleteValue(AppName, false);
+            rk.Close();
+        }
+
+        private bool CheckStartup()
+        {
+            object CheckRegistry;
+            string AppName = Application.ProductName;
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", false);
+            CheckRegistry = rk.GetValue(AppName);
+            rk.Close();
+            return(CheckRegistry != null);
+        }
+
+        private bool StoreStringValue(string ValueStr, string Str)
+        {
+            string StoredValue;
+            RegistryKey rkey = Registry.CurrentUser.CreateSubKey(RegistryApplicationKey);
+            if (rkey != null)
+            {
+                StoredValue = (string)rkey.GetValue(ValueStr, Str + "!");
+                if (Str != StoredValue) rkey.SetValue(ValueStr, Str);
+                return (true);
+            }
+            return false;
+        }
+
+        private string GetStringValue(string ValueStr, string Default)
+        {
+            string Result = Default;
+            RegistryKey rkey = Registry.CurrentUser.OpenSubKey(RegistryApplicationKey);
+            if (rkey != null)
+            {
+                Result = (string)rkey.GetValue(ValueStr, Default);
+            }
+            return (Result);
+        }
+
+        public static string GetExternalIPAndHostName()
+        {
+            try
+            {
+                It.IPNumber = "";
+                string url = "http://checkip.dyndns.org";
+                System.Net.WebRequest req = System.Net.WebRequest.Create(url);
+                System.Net.WebResponse resp = req.GetResponse();
+                System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+                string response = sr.ReadToEnd().Trim();
+                string[] a = response.Split(':');
+                string a2 = a[1].Substring(1);
+                string[] a3 = a2.Split('<');
+                string a4 = a3[0];
+                It.IPNumber = a4;
+                // return a4;
+                var timeout = TimeSpan.FromSeconds(0.2);
+                var task = Dns.GetHostEntryAsync(a4);
+                if (!task.Wait(timeout))
+                {
+                    return ("");
+                }
+                return (task.Result.HostName);
+            }
+            catch
+            {
+                It.timer3.Start();
+                return ("");
+            }
+        }
+
+        public static void UpdateIcons(Icon NewIcon)
+        {
+            if (It.InvokeRequired)
+            {
+                It.BeginInvoke(new Action(delegate
+                {
+                    UpdateIcons(NewIcon);
+                }));
+                return;
+            }
+            else
+            {
+                It.Icon = NewIcon;
+                It.notifyIcon1.Icon = NewIcon;
+            }
+        }
+
+        public void AddressChanged()
+        {
+            ExternalHostName = GetExternalIPAndHostName();
+            if (It.IPNumber != "")
+            {
+                if (It.CheckActiveVPN(It.ExternalHostName))
+                {
+                    It.toolStripStatusLabel1.ForeColor = Color.DarkGreen;                    
+                    if (It.ExternalHostName == "")
+                    {
+                        It.toolStripStatusLabel1.Text = It.IPNumber;
+                        It.notifyIcon1.Text = It.IPNumber;
+                        It.notifyIcon1.BalloonTipText = It.IPNumber;                        
+                    }
+                    else
+                    {
+                        It.toolStripStatusLabel1.Text = It.ExternalHostName;
+                        It.notifyIcon1.Text = It.ExternalHostName;
+                        It.notifyIcon1.BalloonTipText = It.ExternalHostName;
+                    }
+                    UpdateIcons(Properties.Resources.VPN_green);
+                    if (enableSoundsWhenConnectionChangesToolStripMenuItem.Checked)
+                    {
+                        System.Media.SystemSounds.Hand.Play();
+                    }
+                }
+                else
+                {
+                    It.toolStripStatusLabel1.ForeColor = Color.DarkRed;
+                    It.toolStripStatusLabel1.Text = It.ExternalHostName;
+                    It.notifyIcon1.Text = It.ExternalHostName;
+                    It.notifyIcon1.BalloonTipText = It.ExternalHostName;
+                    It.notifyIcon1.ShowBalloonTip(5000);
+                    UpdateIcons(Properties.Resources.VPN_red);
+                    if (enableSoundsWhenConnectionChangesToolStripMenuItem.Checked)
+                    {
+                        System.Media.SystemSounds.Exclamation.Play();
+                    }
+                }
+            }
+            else
+            {
+                UpdateIcons(Properties.Resources.VPN_blue);
+                It.notifyIcon1.BalloonTipText = "";
+                It.toolStripStatusLabel1.ForeColor = Color.Black;
+                It.toolStripStatusLabel1.Text = "";
+                It.notifyIcon1.Text = "";
+            }
+        }
+
+        static void AddressChangedCallback(object sender, EventArgs e)
+        {
+            It.RestartTimer();
+        }
+
+        public bool CheckActiveVPN(String CurrentExternalHostName)
+        {
+            string[] words;
+            int Count;
+            bool VPN = true;
+            String Hosts = It.textBox1.Text;
+            words = Hosts.Split(',');
+            Count = 0;
+            if (words.Length > 0)
+            {
+                while ((VPN) && (Count<words.Length))
+                {
+                    VPN = !(CurrentExternalHostName.Contains(words[Count++]));
+                }
+            }
+            return (VPN);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RestartTimer();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new Action(delegate
+                {
+                    timer1_Tick(sender,e);
+                }));
+                return;
+            }
+            timer1.Stop();
+            AddressChanged();
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                this.Hide();
+            }
+            if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }                
+            RestartTimer();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon1.Visible = false;
+            }
+            RestartTimer();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                // WinForms app
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                // Console app
+                System.Environment.Exit(1);
+            }
+        }
+
+        private void autoStartAtWindowsStartupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetStartup(autoStartAtWindowsStartupToolStripMenuItem.Checked);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            StoreStringValue("NoVPN", textBox1.Text);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Stop();
+            RestartTimer();
+        }
+
+        private void enableSoundsWhenConnectionChangesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StoreBoolValue("PlaySound", enableSoundsWhenConnectionChangesToolStripMenuItem.Checked);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutWindow = new AboutBox1();
+            aboutWindow.Show();
+        }
+    }
+}
